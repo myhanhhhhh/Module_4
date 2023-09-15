ed5370daa218982482afc602f8bcb68864662a87
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-09-15 12:55:04 +0700

    up bai tap

--- ss2_spring_controller/exercices/caculator/src/main/java/com/example/caculator/controller/CaculatorController.java
+++ ss2_spring_controller/exercices/caculator/src/main/java/com/example/caculator/controller/CaculatorController.java
@@ -1,2 +1,41 @@
-package com.example.caculator.controller;public class CaculatorController {
+package com.example.caculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/calculator")
+public class CaculatorController {
+    @GetMapping("")
+    public String showList() {
+        return "calculator";
+    }
+
+    @PostMapping("calculate")
+    public String calculate(RedirectAttributes redirectAttributes, @RequestParam("calculate") String calculate,
+                             @RequestParam double number1, @RequestParam double number2) {
+        if (Objects.equals(calculate, "Addition(+)")) {
+            redirectAttributes.addFlashAttribute("result", number1 + number2);
+        }
+        if (Objects.equals(calculate, "Subtraction(-)")) {
+            redirectAttributes.addFlashAttribute("result", number1 - number2);
+        }
+        if (Objects.equals(calculate, "Mutiplication(*)")) {
+            redirectAttributes.addFlashAttribute("result", number1 * number2);
+        }
+        if (Objects.equals(calculate, "Division(/)")) {
+            if (number2 == 0) {
+                redirectAttributes.addFlashAttribute("result1", "Can't divide by 0");
+            } else {
+                redirectAttributes.addFlashAttribute("result", number1 / number2);
+            }
+        }
+        return "redirect:/calculator";
+    }
 }
--- ss2_spring_controller/exercices/caculator/src/main/webapp/WEB-INF/views/calculator.jsp
+++ ss2_spring_controller/exercices/caculator/src/main/webapp/WEB-INF/views/calculator.jsp
@@ -6,11 +6,28 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/calculator/calculate" method="post">
+    <h2 style="margin-left: 180px">Calculator</h2>
+    <label for="number1">Number 1</label>
+    <input type="number" id="number1" name="number1">
+    <label for="number2">Number 2</label>
+    <input type="number" id="number2" name="number2">
+    <br><br>
+    <input type="submit" name="calculate" value="Addition(+)">
+    <input type="submit" name="calculate" value="Subtraction(-)">
+    <input type="submit" name="calculate" value="Mutiplication(*)">
+    <input type="submit" name="calculate" value="Division(/)">
+    <br><br>
+    Result : <span style="color: green">${result}</span>
+    <span style="color: red">${result1}</span>
+
+</form>
+</body>
 </html>
