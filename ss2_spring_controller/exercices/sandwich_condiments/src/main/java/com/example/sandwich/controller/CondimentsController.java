738082a25ca35671e78659fa9677cc5f27eac6e1
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-09-15 12:53:55 +0700

    up bai tap

--- ss2_spring_controller/exercices/sandwich_condiments/src/main/java/com/example/sandwich/controller/CondimentsController.java
+++ ss2_spring_controller/exercices/sandwich_condiments/src/main/java/com/example/sandwich/controller/CondimentsController.java
@@ -1,2 +1,23 @@
-package com.example.sandwich.controller;public class CondimentsController {
+package com.example.sandwich.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/condiments")
+public class CondimentsController {
+    @GetMapping("")
+    public String showList() {
+        return "list";
+    }
+
+    @PostMapping("/save")
+    public String save(RedirectAttributes redirectAttributes, @RequestParam(name = "condiment", required = false,
+            defaultValue = "0") String[] condiment) {
+        redirectAttributes.addFlashAttribute("message", Arrays.toString(condiment));
+        return "redirect:/condiments";
+    }
 }
