e4da91f26e89bade733eb1c0c22ce7998ea4b0ac
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-09-26 14:21:55 +0700

    up bai tap

--- code_panda/src/main/java/com/codegym/case_study_team_2_study/controller/assignment/AssignmentController.java
+++ code_panda/src/main/java/com/codegym/case_study_team_2_study/controller/assignment/AssignmentController.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Controller
-@RequestMapping("/assignment")
+@RequestMapping("/assignmentt")
 public class AssignmentController {
     @ModelAttribute("assignmentDto")
     public AssignmentDto newAssignmentDto(){
