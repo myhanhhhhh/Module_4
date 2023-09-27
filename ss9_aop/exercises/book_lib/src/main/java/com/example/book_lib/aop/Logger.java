94b352b4c3a44fbc0912502d83ecb99480352b26
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-09-27 23:37:41 +0700

    up

--- ss9_aop/exercises/book_lib/src/main/java/com/example/book_lib/aop/Logger.java
+++ ss9_aop/exercises/book_lib/src/main/java/com/example/book_lib/aop/Logger.java
@@ -1,4 +1,5 @@
 package com.example.book_lib.aop;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,13 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
     public static int count = 0;
+
     @After("execution(* com.example.book_lib.controller.BookController.*(..))")
-    public void count(){
+    public void count() {
         count++;
-        System.out.println("Number Traffic is " +count);
+        System.out.println("Number of book changes " + count);
     }
+
     @After("execution(* com.example.book_lib.controller.BookController.*(..))")
-    public void writeLog(JoinPoint joinPoint){
+    public void writeLog(JoinPoint joinPoint) {
         System.out.println(joinPoint.getSignature().getName());
     }
 }
--- ss9_aop/exercises/book_lib/src/main/java/com/example/book_lib/controller/BookController.java
+++ ss9_aop/exercises/book_lib/src/main/java/com/example/book_lib/controller/BookController.java
@@ -1,13 +1,10 @@
 package com.example.book_lib.controller;
 
-import com.example.book_lib.model.Book;
 import com.example.book_lib.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("book")
